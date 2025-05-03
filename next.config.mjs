0730716a859ecda7d52d37dca84c077e304aba40
Koderkup <koderkup@gmail.com>
2025-05-03 16:16:01 +0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  fallbacks:{
+    document: "/~offline",
+  }
+});
+
+export default withPWA({
+});
