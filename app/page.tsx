e395c6e0c297062aa2376c27b76e476fc74c4f9a
Koderkup <koderkup@gmail.com>
2025-05-14 20:04:53 +0300

    changed background image according theme

--- app/page.tsx
+++ app/page.tsx
@@ -5,48 +5,7 @@ import {Image as ChakraImage} from '@chakra-ui/react'
 export default function Home() {
   return (
     <Box flexGrow={1}>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nemo.
-        Mollitia non, delectus eos sapiente minus, accusantium ipsum laborum,
-        eius nihil culpa facilis consequuntur? Illum tempora explicabo dicta
-        adipisci consequuntur!
-      </Text>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nemo.
-        Mollitia non, delectus eos sapiente minus, accusantium ipsum laborum,
-        eius nihil culpa facilis consequuntur? Illum tempora explicabo dicta
-        adipisci consequuntur!
-      </Text>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nemo.
-        Mollitia non, delectus eos sapiente minus, accusantium ipsum laborum,
-        eius nihil culpa facilis consequuntur? Illum tempora explicabo dicta
-        adipisci consequuntur!
-      </Text>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nemo.
-        Mollitia non, delectus eos sapiente minus, accusantium ipsum laborum,
-        eius nihil culpa facilis consequuntur? Illum tempora explicabo dicta
-        adipisci consequuntur!
-      </Text>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nemo.
-        Mollitia non, delectus eos sapiente minus, accusantium ipsum laborum,
-        eius nihil culpa facilis consequuntur? Illum tempora explicabo dicta
-        adipisci consequuntur!
-      </Text>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nemo.
-        Mollitia non, delectus eos sapiente minus, accusantium ipsum laborum,
-        eius nihil culpa facilis consequuntur? Illum tempora explicabo dicta
-        adipisci consequuntur!
-      </Text>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, nemo.
-        Mollitia non, delectus eos sapiente minus, accusantium ipsum laborum,
-        eius nihil culpa facilis consequuntur? Illum tempora explicabo dicta
-        adipisci consequuntur!
-      </Text>
+     
     </Box>
   );
 }
